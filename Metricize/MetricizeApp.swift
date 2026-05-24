50d907e7248f57db0d697f029a524d8d93892505
James Oakes <james.d.oakes@outlook.com>
2026-05-23 20:29:25 -0400

    Initial Commit

---
+++ Metricize/MetricizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetricizeApp.swift
+//  Metricize
+//
+//  Created by James on 5/23/26.
+//
+
+import SwiftUI
+
+@main
+struct MetricizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
